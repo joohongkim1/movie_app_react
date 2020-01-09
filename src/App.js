043ee3280276fff6fab764f2e24acbc00ef47c2b
joohongkim1 <wnghd9938@naver.com>
2020-01-09 17:48:47 +0900

    map Recap

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function Food({ name, image }) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={image} />
+      {/* image element 는 alt prop 이 반드시 필요 */}
+      <img src={image} alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: "Kimchi",
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR720x0%2F%3Ffname%3Dhttps%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F80f442b593b14aebb8af3d5a0e709aa6.jpg&f=1&nofb=1"
   },
   {
+    id: 2,
     name: "ramen",
     image:
       "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpds.joins.com%2Fnews%2Fcomponent%2Fhtmlphoto_mmdata%2F201412%2F21%2Fhtm_20141221114418c010c011.jpg&f=1&nofb=1"
@@ -33,7 +36,8 @@ function App() {
       {/* map 은 function 을 취해서 array 의 각 item 에 function 을 적용한다. */}
       {/* dish 는 object  */}
       {foodILike.map(dish => (
-        <Food name={dish.name} image={dish.image} />
+        // key 는 Food component 에서 사용되지 않고, react 내부에서만 사용
+        <Food key={dish.id} name={dish.name} image={dish.image} />
       ))}
     </div>
   );

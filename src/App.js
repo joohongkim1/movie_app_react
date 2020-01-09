8857f063a2dc2434197607aeed67a5e83ee171d2
joohongkim1 <wnghd9938@naver.com>
2020-01-09 11:09:21 +0900

    Resuable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// component => HTML 을 반환하는 함수
+
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      {/* Food component 에 kimchi 라는 value 로
+      prop(property ) name 설정 */}
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="rice" />
     </div>
   );
 }

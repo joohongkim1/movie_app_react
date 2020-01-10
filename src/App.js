db4eeeac43dcb5b2f20048e8690539283feb0bc5
joohongkim1 <wnghd9938@naver.com>
2020-01-10 16:36:11 +0900

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 // component => HTML 을 반환하는 함수
 
-function Food({ name, image }) {
+function Food({ name, image, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
       {/* image element 는 alt prop 이 반드시 필요 */}
       <img src={image} alt={name} />
+      <h4>rating: {rating}</h4>
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
     image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR720x0%2F%3Ffname%3Dhttps%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F80f442b593b14aebb8af3d5a0e709aa6.jpg&f=1&nofb=1"
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR720x0%2F%3Ffname%3Dhttps%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F80f442b593b14aebb8af3d5a0e709aa6.jpg&f=1&nofb=1",
+    rating: 5
   },
   {
     id: 2,
     name: "ramen",
     image:
-      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpds.joins.com%2Fnews%2Fcomponent%2Fhtmlphoto_mmdata%2F201412%2F21%2Fhtm_20141221114418c010c011.jpg&f=1&nofb=1"
+      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpds.joins.com%2Fnews%2Fcomponent%2Fhtmlphoto_mmdata%2F201412%2F21%2Fhtm_20141221114418c010c011.jpg&f=1&nofb=1",
+    rating: 4.9
   }
 ];
 
@@ -37,7 +47,12 @@ function App() {
       {/* dish 는 object  */}
       {foodILike.map(dish => (
         // key 는 Food component 에서 사용되지 않고, react 내부에서만 사용
-        <Food key={dish.id} name={dish.name} image={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          image={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );

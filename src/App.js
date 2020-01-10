f3e5817ce7a79a95d069290303d708ee5f08a49b
joohongkim1 <wnghd9938@naver.com>
2020-01-10 17:02:13 +0900

    Class Components and State

--- src/App.js
+++ src/App.js
@@ -2,60 +2,29 @@ import React from "react";
 import PropTypes from "prop-types";
 // component => HTML 을 반환하는 함수
 
-function Food({ name, image, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      {/* image element 는 alt prop 이 반드시 필요 */}
-      <img src={image} alt={name} />
-      <h4>rating: {rating}</h4>
-    </div>
-  );
-}
+class App extends React.Component {
+  // react 는 자동적으로 class component 의 render method 를 실행
+  // state 는 object 이며, component 의 data 를 넣을 공간이 있으며, 데이터가 변한다.
+  state = {
+    count: 0
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg1.daumcdn.net%2Fthumb%2FR720x0%2F%3Ffname%3Dhttps%3A%2F%2Ft1.daumcdn.net%2Fliveboard%2Fdailylife%2F80f442b593b14aebb8af3d5a0e709aa6.jpg&f=1&nofb=1",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "ramen",
-    image:
-      "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpds.joins.com%2Fnews%2Fcomponent%2Fhtmlphoto_mmdata%2F201412%2F21%2Fhtm_20141221114418c010c011.jpg&f=1&nofb=1",
-    rating: 4.9
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-];
-
-function App() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-      {/* Food component 에 kimchi 라는 value 로
-      prop(property ) name 설정 */}
-      {/* <Food fav="kimchi" /> */}
-      {/* map 은 function 을 취해서 array 의 각 item 에 function 을 적용한다. */}
-      {/* dish 는 object  */}
-      {foodILike.map(dish => (
-        // key 는 Food component 에서 사용되지 않고, react 내부에서만 사용
-        <Food
-          key={dish.id}
-          name={dish.name}
-          image={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
 }
 
 export default App;

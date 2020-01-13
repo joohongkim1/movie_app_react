15b489912da431758252e140d84a10e286fb9611
joohongkim1 <wnghd9938@naver.com>
2020-01-13 09:43:26 +0900

    Updates

---
+++ precache-manifest.c82343dfbb1cdb90111752e1ddfc38e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07309d8f54bad2b896f8fd263b9dd2be",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "34ddd419b77455b3bbc2",
+    "url": "/movie_app_react/static/css/main.2b9b12cc.chunk.css"
+  },
+  {
+    "revision": "206dd94248b29d740344",
+    "url": "/movie_app_react/static/js/2.f65e7f63.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_react/static/js/2.f65e7f63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "34ddd419b77455b3bbc2",
+    "url": "/movie_app_react/static/js/main.d87facfd.chunk.js"
+  },
+  {
+    "revision": "9dade55e9b6cc25e9676",
+    "url": "/movie_app_react/static/js/runtime-main.09561f9e.js"
+  }
+]);

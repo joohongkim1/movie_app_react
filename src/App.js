8f9cd3f8476705a1696ba4861a77c2e7824570d3
joohongkim1 <wnghd9938@naver.com>
2020-01-13 11:34:23 +0900

    Getting Ready for the Router

--- src/App.js
+++ src/App.js
@@ -1,59 +1,7 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./Movie";
-import "./App.css";
 
-class App extends React.Component {
-  state = {
-    isLoading: true,
-    movies: []
-  };
-  getMovies = async () => {
-    // async => await 을 사용하려면 필요
-    // 이 함수가 비동기라는 것을 알려줌
-    // axios 가 끝날 때까지 기다렸다가 계속 진행
-    const {
-      data: {
-        data: { movies }
-      }
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoading: false });
-  };
-  componentDidMount() {
-    // setTimeout => delay function
-    // setTimeout(() => {
-    //   this.setState({ isLoading: false });
-    // }, 6000); 6초 뒤 isLoading 의 상태가 false 로 변경
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader_text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              ></Movie>
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return <span>something</span>;
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,18 +10,16 @@ function Movie({ year, title, summary, poster, genres }) {
     <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie_data">
-        <h3 className="movie_title" style={{ backgroundColor: "red" }}>
-          {title}
-        </h3>
+        <h3 className="movie_title">{title}</h3>
         <h5 className="movie_year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie_genres">
           {genres.map((genre, index) => (
             <li key={index} className="genres_genre">
               {genre}
             </li>
           ))}
         </ul>
-        <p className="movie_summary">{summary.slice(0, 140)}...</p>
+        <p className="movie_summary">{summary.slice(0, 200)}...</p>
       </div>
     </div>
   );

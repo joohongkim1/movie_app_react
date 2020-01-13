8cdea6e9c38d7a033a41664f93f943cffa0204b8
joohongkim1 <wnghd9938@naver.com>
2020-01-13 12:15:23 +0900

    Building the Navigation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
-import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
 // router : url 을 통해 component 를 불러온다.
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       {/* 라우터가 '/' 를 인식해서 home 과 about 을 동시에 렌더링한다. */}
       {/* <Route path="/" component={Home} />
       <Route path="/about" component={About} /> */}
-
+      <Navigation />
       {/* exact={true} => path와 일치하는 url 일 때만 인식 */}
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
